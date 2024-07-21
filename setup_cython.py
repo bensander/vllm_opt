86a5cadc211b6b82efc1937efa9a7e2d864e3645
Ben Sander <ben.sander@amd.com>
2024-07-21 22:35:29 +0000

    Add cython.
    $ pip3 install cython
    
    python3 setup_cython.py build_ext --inplace

---
+++ setup_cython.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+from Cython.Build import cythonize
+
+import Cython.Compiler.Options
+Cython.Compiler.Options.annotate = True
+
+infiles = []
+
+infiles += [
+	"vllm/engine/llm_engine.py",
+	"vllm/transformers_utils/detokenizer.py",
+        "vllm/engine/output_processor/single_step.py",
+        "vllm/outputs.py",
+        "vllm/engine/output_processor/stop_checker.py",
+    ]
+
+infiles += [
+        "vllm/core/scheduler.py",
+        #"vllm/sequence.py",
+        "vllm/core/block_manager_v1.py",
+        ]
+
+
+setup(
+        ext_modules=cythonize(infiles, annotate=True, force=True,
+            compiler_directives= {
+                'language_level' : "3",
+                'infer_types' : True
+                })
+)
